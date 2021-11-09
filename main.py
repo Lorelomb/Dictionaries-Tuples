2430b7c6eb13924417ed5f1cb681ddd304dd202e
2020326 <2020326@brunel.ac.uk>
2021-11-09 14:50:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# user_input = input("Create a list of numbers that containing odd and even numbers; ")
+#
+# user_list = user_input.split()
+# new_list = user_list[:]
+#
+# for number in user_list:
+#     if int(number) % 2 == 0:
+#         user_list.remove(number)
+#
+# print(user_list)
+
+# tupleList = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
+#
+# newTuple = []
+#
+# for t in tupleList:
+#     if t:
+#         newTuple.append(t)
+# print(newTuple)
+
+# my_list = [5,6,7]
+# my_tuple = (5,6,"superman",[89,"red"])
+# my_dictionary = {"Name" : "Tom","Age":19}
+# print(my_dictionary["Name"])
+#
+# for key in my_dictionary.keys():
+#     print(key)
+
+# my_dictionary = {['Name:']}
+#
+# question = input("What is your Name :")
+# if input:
+#     my_dictionary.append(input())
+#     print(my_dictionary['Name'])
+#
+
+import time
+
+question = ("hello")
+time.sleep(3)
+print(question)
